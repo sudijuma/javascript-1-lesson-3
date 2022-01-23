acc488c95d515ab2b67ad7c58442d1c8216fb6d8
Hesham <heshamelmasry1992@gmail.com>
2022-01-23 20:29:04 +0100

    Add the questions for lesson 3

---
+++ loading_multiple_javascript_files/js/script1.js
@@ -0,0 +1,2 @@
+// Declare a variable called pet with string value dog
+// create a function called back which console log Woof! Woof!
---
+++ loading_multiple_javascript_files/js/script2.js
@@ -0,0 +1,7 @@
+// this is in script2.js
+
+// 1. console log the variable pet
+// dog
+
+// 2. use the function bark()
+// Woof!
